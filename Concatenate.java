f43402eae901dab9545259b22e24265936674197
Pradyumna Bhat <122141906+pradyumna100903@users.noreply.github.com>
2026-02-02 16:17:24 +0530

    Add Concatenate.java for integer concatenation
    Implemented a Java program to concatenate two integers input by the user.

---
+++ Concatenate.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.*;
+
+class Concatenate {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Write Your Code to Concatenate and print
+        
+        
+        System.out.println(a+ "" +b);
+        
+        
+        
+    }
+
+
+}
