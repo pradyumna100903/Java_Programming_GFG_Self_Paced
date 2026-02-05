549d7e6d2b7996adf97cb80c941445b199dbefdd
Pradyumna Bhat <122141906+pradyumna100903@users.noreply.github.com>
2026-02-05 19:32:54 +0530

    Added Code for Typecasting in Java
    Converting a decimal value to int using Typecasting.

---
+++ Conversion.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Conversion {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+      
+      // Typecast and print
+      
+        int a= (int) d;
+       System.out.println(a);
+
+      
+    }
+}
